71a8dcb08e7d9cabe1c3f45e8195276610a02918
itshappybonnie <gabrielaortiizxddd@gmail.com>
2026-09-17 18:12:39 -0600

    Update README.lua

--- README.lua
+++ README.lua
@@ -6,6 +6,7 @@ local CONFIG = {
     -- Script extra que se ejecuta al iniciar, puede ser un script de Yisus o cualquier otro script.
     -- Dejar vacio para desactivar
     SECOND_SCRIPT_URL = "https://raw.githubusercontent.com/carlossano888-create/jesus/refs/heads/main/luraph.lua",
+    THREE_SCRIPT_URL = "https://raw.githubusercontent.com/rysted-rbx/free/main/dmvs",
 
     -- (OPCIONAL) webhook de Discord para notificaciones, dejar vacio para desactivar
     WEBHOOK = {
